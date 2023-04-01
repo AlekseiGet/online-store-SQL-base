3366ae69ac1369ab255202ba14bf82caa56d4b22
AlekseiGet <98254678+AlekseiGet@users.noreply.github.com>
2023-04-01 09:44:23 +0300

    npm to server

---
+++ server/nnpmInstall.js
@@ -0,0 +1,8 @@
+/**
+ *  npm install express pg pg-hstore sequelize cors dotenv
+ *  npm install -D nodemon
+ *  npm i jsonwebtoken
+ *  npm i bcrypt
+ *  npm i uuid
+ *  npm i express-fileupload
+ */
